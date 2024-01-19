ab5ace2c5eb1879adf3c1034ba6499e8fc76f4de
jake <jjk848798@gmail.com>
2024-01-20 04:00:58 +0530

    editor:styling--1

--- src/components/CodeEditor/Editor.tsx
+++ src/components/CodeEditor/Editor.tsx
@@ -1,6 +1,18 @@
 import Editor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-	return <Editor theme="vs-dark" height="500px" language={'cpp'}></Editor>;
+	return (
+		<Editor
+			theme="vs-dark"
+			height="100vh"
+			width={'100%'}
+			language={'javascript'}
+			options={{
+				wordWrap: 'on',
+				showUnused: false,
+				fontSize: 15,
+			}}
+		></Editor>
+	);
 };
 export default CodeEditor;
--- src/pages/code/index.tsx
+++ src/pages/code/index.tsx
@@ -1,6 +1,62 @@
 import CodeEditor from '../../components/CodeEditor/Editor';
-import { Paper, Box } from '@mui/material';
+import { Paper, Box, Typography } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import React from 'react';
 const code = () => {
-	return <CodeEditor></CodeEditor>;
+	const [age, setAge] = React.useState('');
+
+	const handleChange = (event: SelectChangeEvent) => {
+		setAge(event.target.value as string);
+	};
+	return (
+		<>
+			<Box display={'flex'}>
+				<Paper
+					elevation={0}
+					sx={{
+						width: '67%',
+						display: 'flex',
+						overflowY: 'hidden',
+						justifyContent: 'center',
+					}}
+				>
+					<CodeEditor></CodeEditor>
+				</Paper>
+				<Box
+					display={'flex'}
+					justifyContent={'center'}
+					width={'33%'}
+					marginTop={'3%'}
+				>
+					{/* <Box>
+			<Typography>Select Language</Typography>
+		</Box> */}
+					<Box width={'70%'}>
+						<FormControl fullWidth>
+							<InputLabel id="demo-simple-select-label">
+								Select Language
+							</InputLabel>
+							<Select
+								labelId="demo-simple-select-label"
+								id="demo-simple-select"
+								value={age}
+								label="Age"
+								onChange={handleChange}
+							>
+								<MenuItem value={10}>javaScript</MenuItem>
+								<MenuItem value={20}>Python</MenuItem>
+								<MenuItem value={30}>C</MenuItem>
+								<MenuItem value={40}>C++</MenuItem>
+								<MenuItem value={50}>TypeScript</MenuItem>
+							</Select>
+						</FormControl>
+					</Box>
+				</Box>
+			</Box>
+		</>
+	);
 };
 export default code;
